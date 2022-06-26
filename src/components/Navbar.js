d5d7fc305b9741597d80db6fa5106c81e9c94faa
MatthewReed17 <reedmj17@gmail.com>
2022-06-26 18:01:51 -0500

    Added Search page to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,6 +12,9 @@ const Navbar = () => {
           <li>
             <Link to="/about">About</Link>
           </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
         </ul>
       </div>
     </nav>
